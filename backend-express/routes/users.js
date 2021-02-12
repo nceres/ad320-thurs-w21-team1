240626c6ab398d4def37684adb9de92a4457d96a
Dale Berg <dale.brg@gmail.com>
2021-02-11 22:07:58 -0800

    update query util and add some examples

--- backend-express/routes/users.js
+++ backend-express/routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const queryUtil = require("../db-utils/queryUtil");
 
-/* GET users listing. */
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // This is just an example of how this will work. We should delete this whole file
+  queryUtil.query("select * from test_hotdog").then(result => res.send(result));
 });
 
 module.exports = router;
--- frontend-react/src/Home.js
+++ frontend-react/src/Home.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 
 class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { apiResponse: "" };
+    }
+
+    callBackend() {
+        console.log("api called");
+        fetch("http://localhost:4000/users")
+            .then(res => res.text())
+            .then(res => this.setState({ apiResponse: res }))
+            .catch(err => console.log(err));
+    }
+
+    componentWillMount() {
+        this.callBackend();
+    }
+
     render() {
         return (
             <div>
+                <p className="App-intro">{this.state.apiResponse}</p>
                 <h2>Hey, you're home!</h2>
                 <p>This is a bunch of text!</p>
                 <p>This a bunch more text!</p>
