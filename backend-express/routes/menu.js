8130cbfebb1d6a774996e615faf48bec1df03d1d
Dale Berg <dale.brg@gmail.com>
2021-02-15 17:03:21 -0800

    update form, db structure

--- backend-express/routes/menu.js
+++ backend-express/routes/menu.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router();
 const queryUtil = require("../db-utils/queryUtil");
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    queryUtil.query("select hotdog_name from Hotdog")
-        .then(result => res.json(result.map(result => result.hotdog_name)));
+    queryUtil.query("select hotdog_id, hotdog_name from Hotdog")
+        .then(result => res.send(result));
 });
 
 module.exports = router;
--- frontend-react/src/Menu.js
+++ frontend-react/src/Menu.js
@@ -4,25 +4,25 @@ import {FormControl, FormGroup, ControlLabel, HelpBlock, Checkbox, Radio, Button
 
 class Menu extends React.Component {
 
-    state = {menuItems: []}
+    state = { menuItems: [], disabled_id: 1 }
 
     componentDidMount() {
         fetch("/menu")
             .then(res => res.json())
-            .then(menuItems => this.setState({menuItems}));
+            .then(menuItems => this.setState({menuItems})).then(ignored => console.log(this.state));
     }
 
     render() {
         return (
             <div>
-                <h1>Users</h1>
+                <h1>Menu!</h1>
                 <Form>
                 {this.state.menuItems.map(menuItem =>
-                        <div key={"checkbox"} className="mb-3">
-                            <Form.Check type={"checkbox"} id={`check-api-radio`}>
-                                <Form.Check.Input type={"checkbox"} isValid/>
-                                <Form.Check.Label>{`Custom api`}</Form.Check.Label>
-                                <Form.Control.Feedback type="valid">You did it!</Form.Control.Feedback>
+                        <div key={menuItem.hotdog_id} className="mb-3">
+                            <Form.Check type={"checkbox"} id={menuItem.hotdog_id}>
+                                <Form.Check.Input disabled={menuItem.hotdog_id === this.state.disabled_id} type={"checkbox"} isValid/>
+                                <Form.Check.Label>{menuItem.hotdog_name}</Form.Check.Label>
+                                <Form.Control.Feedback type="valid">We can display a dynamic message here!</Form.Control.Feedback>
                             </Form.Check>
                         </div>
                 )}
