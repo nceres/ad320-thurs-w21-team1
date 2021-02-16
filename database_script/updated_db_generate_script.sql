334fa33c57395cbdeaed8b6c4f2c0c9d301a427f
Dale Berg <dale.brg@gmail.com>
2021-02-15 17:04:05 -0800

    restructure db

---
+++ database_script/updated_db_generate_script.sql
@@ -0,0 +1,209 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema HotdogCart
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `HotdogCart` ;
+
+-- -----------------------------------------------------
+-- Schema HotdogCart
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `HotdogCart` DEFAULT CHARACTER SET utf8 ;
+USE `HotdogCart` ;
+
+-- -----------------------------------------------------
+-- Table `HotdogCart`.`Location`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `HotdogCart`.`Location` ;
+
+CREATE TABLE IF NOT EXISTS `HotdogCart`.`Location` (
+  `location_id` INT NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+  `name` VARCHAR(45) NULL COMMENT 'location name',
+  `address` VARCHAR(45) NOT NULL COMMENT 'location address',
+  `phone` VARCHAR(10) NULL COMMENT 'location phone',
+  PRIMARY KEY (`location_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `HotdogCart`.`Person_Role`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `HotdogCart`.`Person_Role` ;
+
+CREATE TABLE IF NOT EXISTS `HotdogCart`.`Person_Role` (
+  `person_role_id` INT NOT NULL AUTO_INCREMENT,
+  `role_name` VARCHAR(45) NULL,
+  PRIMARY KEY (`person_role_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `HotdogCart`.`Person`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `HotdogCart`.`Person` ;
+
+CREATE TABLE IF NOT EXISTS `HotdogCart`.`Person` (
+  `person_id` INT NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+  `first_name` VARCHAR(45) NULL COMMENT 'customer first name',
+  `last_name` VARCHAR(45) NULL COMMENT 'customer last name',
+  `address` VARCHAR(100) NULL COMMENT 'customer address',
+  `email` VARCHAR(45) NULL COMMENT 'customer email',
+  `phone` VARCHAR(10) NULL COMMENT 'customer phone',
+  `location_id` INT NULL,
+  `role_id` INT NULL,
+  `hashed_password` VARCHAR(45) NULL,
+  PRIMARY KEY (`person_id`),
+  INDEX `peron_role_id_idx` (`role_id` ASC) VISIBLE,
+  INDEX `location_id_idx` (`location_id` ASC) VISIBLE,
+  CONSTRAINT `fk_person_location`
+    FOREIGN KEY (`location_id`)
+    REFERENCES `HotdogCart`.`Location` (`location_id`)
+    ON DELETE NO ACTION
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_person_role`
+    FOREIGN KEY (`role_id`)
+    REFERENCES `HotdogCart`.`Person_Role` (`person_role_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `HotdogCart`.`Hotdog`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `HotdogCart`.`Hotdog` ;
+
+CREATE TABLE IF NOT EXISTS `HotdogCart`.`Hotdog` (
+  `hotdog_id` INT NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+  `hotdog_name` VARCHAR(45) NULL COMMENT 'hotdog name',
+  `hotdog_image` VARCHAR(45) NULL COMMENT 'link to image',
+  PRIMARY KEY (`hotdog_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `HotdogCart`.`Order`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `HotdogCart`.`Order` ;
+
+CREATE TABLE IF NOT EXISTS `HotdogCart`.`Order` (
+  `order_id` INT NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+  `date` DATETIME NULL COMMENT 'order date',
+  `location_id` INT NOT NULL COMMENT 'foreign key',
+  `person_id` INT NOT NULL COMMENT 'foreign key',
+  PRIMARY KEY (`order_id`),
+  INDEX `fk_Order_Location1_idx` (`location_id` ASC) VISIBLE,
+  INDEX `person_id_idx` (`person_id` ASC) VISIBLE,
+  CONSTRAINT `fk_order_location`
+    FOREIGN KEY (`location_id`)
+    REFERENCES `HotdogCart`.`Location` (`location_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_order_person`
+    FOREIGN KEY (`person_id`)
+    REFERENCES `HotdogCart`.`Person` (`person_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `HotdogCart`.`Order_Items`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `HotdogCart`.`Order_Items` ;
+
+CREATE TABLE IF NOT EXISTS `HotdogCart`.`Order_Items` (
+  `order_items_id` INT NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+  `quantity` INT NULL COMMENT 'order quantity',
+  `order_id` INT NULL COMMENT 'foreign key',
+  `hotdog_id` INT NULL COMMENT 'foreign key',
+  PRIMARY KEY (`order_items_id`),
+  INDEX `fk_Order Item_Order1_idx` (`order_id` ASC) VISIBLE,
+  INDEX `fk_Order Items_Hotdog1_idx` (`hotdog_id` ASC) VISIBLE,
+  CONSTRAINT `fk_Order Item_Order1`
+    FOREIGN KEY (`order_id`)
+    REFERENCES `HotdogCart`.`Order` (`order_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Order Items_Hotdog1`
+    FOREIGN KEY (`hotdog_id`)
+    REFERENCES `HotdogCart`.`Hotdog` (`hotdog_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `HotdogCart`.`Permission`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `HotdogCart`.`Permission` ;
+
+CREATE TABLE IF NOT EXISTS `HotdogCart`.`Permission` (
+  `permission_id` INT NOT NULL AUTO_INCREMENT,
+  `permission_name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`permission_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `HotdogCart`.`Role_Permission`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `HotdogCart`.`Role_Permission` ;
+
+CREATE TABLE IF NOT EXISTS `HotdogCart`.`Role_Permission` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `role_id` INT NULL,
+  `permission_id` INT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `role_id_idx` (`role_id` ASC) VISIBLE,
+  INDEX `permission_id_idx` (`permission_id` ASC) VISIBLE,
+  CONSTRAINT `fk_role_perm_role`
+    FOREIGN KEY (`role_id`)
+    REFERENCES `HotdogCart`.`Person_Role` (`person_role_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_roleperm_permission_id`
+    FOREIGN KEY (`permission_id`)
+    REFERENCES `HotdogCart`.`Permission` (`permission_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `HotdogCart`.`Disabled_Menu_Items`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `HotdogCart`.`Disabled_Menu_Items` ;
+
+CREATE TABLE IF NOT EXISTS `HotdogCart`.`Disabled_Menu_Items` (
+  `person_id` INT NOT NULL,
+  `hotdog_id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  INDEX `person_id_idx` (`person_id` ASC) VISIBLE,
+  INDEX `hotdog_id_idx` (`hotdog_id` ASC) VISIBLE,
+  CONSTRAINT `fk_vendor_disabled`
+    FOREIGN KEY (`person_id`)
+    REFERENCES `HotdogCart`.`Person` (`person_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_disabled_hotdog`
+    FOREIGN KEY (`hotdog_id`)
+    REFERENCES `HotdogCart`.`Hotdog` (`hotdog_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+INSERT INTO Hotdog VALUES (null, "Classic", "none");
+INSERT INTO Hotdog VALUES (null, "Seattle dog", "none");
+INSERT INTO Hotdog VALUES (null, "Frankfurter", "none");
+INSERT INTO Hotdog VALUES (null, "Chicago dog", "none");
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
