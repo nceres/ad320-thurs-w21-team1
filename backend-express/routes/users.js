efe308876a5d155d3467a4a1b37d3c578225eb21
Dagmawi Girma Demissie <dagimethiopia17@gmail.com>
2021-02-22 20:52:30 -0800

    Added a query for Admins

--- backend-express/routes/users.js
+++ backend-express/routes/users.js
@@ -5,7 +5,8 @@ const queryUtil = require("../db-utils/queryUtil");
 
 router.get('/', function(req, res, next) {
   // This is just an example of how this will work. We should delete this whole file
-  queryUtil.query("select * from Hotdog").then(result => res.send(result));
+  queryUtil.query("SELECT concat(first_name,' ' , last_name) AS Admin_Name FROM hotdogcart.person JOIN hotdogcart.person_role ON person.role_id = person_role.person_role_id WHERE person_role.role_name = 'Admin'")
+  .then(result => res.send(result));
 });
 
 module.exports = router;
