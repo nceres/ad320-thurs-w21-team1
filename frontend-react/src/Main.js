3876529c369d2bc045588c8fd7d174ca6a62fcd5
Dale Berg <dale.brg@gmail.com>
2021-03-01 11:42:18 -0800

    readd buttons

--- frontend-react/src/Main.js
+++ frontend-react/src/Main.js
@@ -4,7 +4,7 @@ import {
     NavLink,
     HashRouter
 } from "react-router-dom";
-
+import {Button, ButtonGroup} from "react-bootstrap";
 import Home from './Home';
 import Menu from './Menu';
 import Contact from './Contact';
@@ -12,17 +12,37 @@ import MapContainer from './MapContainer';
 import Admin from './Admin';
 
 class Main extends React.Component {
+
+    state = {
+        selectedView: "customer"
+    }
+
+    handleOnClick = (viewClicked) => {
+        this.setState({selectedView: viewClicked});
+        //reload app to apply view
+    }
+
+    componentDidMount() {
+        console.log(this.state.selectedView)
+    }
+
     render() {
         return (
             <HashRouter>
                 <div className="container">
                     <h1 className="title">Dog Eat Dog World</h1>
+                    <ButtonGroup className="buttonGroup">
+                        <Button variant="primary" onClick={() => this.handleOnClick("customer")}>Customer</Button>
+                        <Button variant="info" onClick={() => this.handleOnClick("vendor")}>Vendor</Button>
+                        <Button variant="success" onClick={() => this.handleOnClick("admin")}>Admin</Button>
+                    </ButtonGroup>
                     <ul className="header">
                         <li><NavLink exact to="/">Home</NavLink></li>
                         <li><NavLink to="/menu">Menu</NavLink></li>
                         <li><NavLink to="/contact">Contact</NavLink></li>
                         <li><NavLink to="/mapContainer">Map</NavLink></li>
-                        <li><NavLink to="/admin">Admin</NavLink></li>
+                        {this.state.selectedView === "admin" && <li><NavLink to="/admin">Admin</NavLink></li>
+                        }
                     </ul>
                     <div className="content">
                         <Route exact path="/" component={ Home }></Route>
