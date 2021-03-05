1a791f25e4d87a34f3c275867c019c3de6170725
Dale Berg <dale.brg@gmail.com>
2021-03-04 19:44:01 -0800

    add logging view

--- backend-express/routes/logging.js
+++ backend-express/routes/logging.js
@@ -2,9 +2,14 @@ var express = require('express');
 var router = express.Router();
 const queryUtil = require("../db-utils/queryUtil");
 
-router.post('/', function(req, res, next) {
+router.post('/', function(req, res) {
     console.log(req.body)
     queryUtil.query("INSERT INTO Logs VALUES (NULL, \'" + req.body.logline + "\', \'" + queryUtil.getDate() + "\')")
+        .then(result => res.send(result));
+});
+
+router.get('/', function(req, res) {
+    queryUtil.query("SELECT log_line, log_timestamp from Logs")
         .then(result => res.send(result));
 });
 
--- frontend-react/src/Admin.js
+++ frontend-react/src/Admin.js
@@ -1,25 +1,39 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 
 class Admin extends React.Component {
 
+    state = {
+        logItems: []
+    }
+
+    componentDidMount() {
+        fetch("/logging")
+            .then(res => res.json())
+            .then(logItems => this.setState({logItems}))
+            .then(ignored => console.log(this.state)); // remove this log statement at some point
+    }
+
     render() {
         return (
-            
-     
-            <form>
-                <label>
-                <center>New Vendor Account<br /><br /></center>
-                Vendor Name: <input type = "text" />
-                Vendor Username: <input type = "text" /><br /><br />
-                Vendor Role: <input type = "text" />
-                Vendor Password: <input type = "password" /><br /><br />
-                <input type = "button" value = "Submit" />
-                </label>
-            </form>
+            <div>
+                <form>
+                    <label>
+                        <center>New Vendor Account<br/><br/></center>
+                        Vendor Name: <input type="text"/>
+                        Vendor Username: <input type="text"/><br/><br/>
+                        Vendor Role: <input type="text"/>
+                        Vendor Password: <input type="password"/><br/><br/>
+                        <input type="button" value="Submit"/>
+                    </label>
+                </form>
+                <p className="consoleLog">
+                    {this.state.logItems.map(item => <div>{item.log_timestamp + '\t' + item.log_line}</div>)}
+                </p>
+            </div>
         )
     }
 }
-      
-  
+
+
 export default Admin;
