64753fbd60e82708d37c4b9473befd949ec786be
Dale Berg <dale.brg@gmail.com>
2021-03-05 21:49:02 -0800

    admins can add items now

--- frontend-react/src/Admin.js
+++ frontend-react/src/Admin.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
-import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
+import {Form, Nav, Button} from 'react-bootstrap';
+import logHelper from "./utils";
 
 
 class Admin extends React.Component {
 
     state = {
-        logItems: [], tabKey: "1"
+        logItems: [],
+        tabKey: "1",
+        newHotdogName: "",
+        newHotdogImgUrl: "",
+        newHotdogPrice: 0,
     }
 
     componentDidMount() {
@@ -20,17 +25,53 @@ class Admin extends React.Component {
         console.log(this.state.tabKey === "1")
     }
 
+    createNewVendor = () => {
+        logHelper({adminAction: "Admin created a new vendor!"})
+        console.log("vendor form created");
+    }
+
     showNewVendorForm() {
-        return <form>
+        return <form onSubmit={this.createNewVendor}>
             <label>
                 <center>New Vendor Account<br/><br/></center>
                 Vendor Name: <input type="text"/>
-                Vendor Username: <input type="text"/><br/><br/>
-                Vendor Role: <input type="text"/>
-                Vendor Password: <input type="password"/><br/><br/>
+                Vendor email: <input type="text"/><br/><br/>
                 <input type="button" value="Submit"/>
             </label>
         </form>
+    }
+
+    addMenuItem = () => {
+        logHelper({adminAction: "Admin created a new hotdog!"}) // TODO add some details here
+        const newHotDog = {
+            hotdog_name: this.state.newHotdogName,
+            hotdog_image: this.state.newHotdogImgUrl,
+            hotdog_price: this.state.newHotdogPrice
+        }
+        fetch("/menu", {
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(newHotDog)
+        }).then(result => result.json())
+    }
+
+    showAddMenuItemForm() {
+        return <Form onSubmit={this.addMenuItem}>
+            <Form.Label>Hotdog Name</Form.Label>
+            <Form.Control placeholder="some name" onChange={(e) => this.setState({newHotdogName: e.target.value})}
+                          required/>
+            <Form.Label>Image Url</Form.Label>
+            <Form.Control placeholder="some name" onChange={(e) => this.setState({newHotdogImgUrl: e.target.value})}
+                          required/>
+            <Form.Label>Price</Form.Label>
+            <Form.Control type="text" placeholder="decimal format, e.g. 1.20"
+                          onChange={(e) => this.setState({newHotdogPrice: e.target.value})} required/>
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
     }
 
     showAdminLogConsole() {
@@ -45,10 +86,11 @@ class Admin extends React.Component {
                 <Nav fill variant="tabs" className="adminBar">
                     <Nav.Link eventKey={1} onSelect={this.handleSelect}>Add Vendor</Nav.Link>
                     <Nav.Link eventKey={2} onSelect={this.handleSelect}>View Console</Nav.Link>
-                    <Nav.Link eventKey={3} onSelect={this.handleSelect}>whatever</Nav.Link>
+                    <Nav.Link eventKey={3} onSelect={this.handleSelect}>Add Menu Item</Nav.Link>
                 </Nav>
                 {this.state.tabKey === "1" && this.showNewVendorForm()}
                 {this.state.tabKey === "2" && this.showAdminLogConsole()}
+                {this.state.tabKey === "3" && this.showAddMenuItemForm()}
             </div>
         )
     }
